c9523a245b5b5014e211dab5fb997b56958c9798
Fang Suqi <130955391+fangsu7@users.noreply.github.com>
2024-12-10 14:19:06 +0800

    Add files via upload

---
+++ Project-Code/src/main/resources/static/js/blog.js
@@ -0,0 +1,7 @@
+document.getElementById("South").addEventListener("click", function() {
+    window.location.href = "https://south66666666.github.io/"; // 替换为您想要跳转的网页URL
+});
+
+function redirectToPage(url) {
+    window.location.href = url;
+}
---
+++ Project-Code/src/main/resources/static/js/introduction.js
@@ -0,0 +1,3 @@
+function redirectToData(url) {
+    window.location.href = url;
+}
